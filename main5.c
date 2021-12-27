3c6ae31d250df16d3e5409f560a912721af53f1e
yura <yura.nabokov1990@mail.ru>
2021-12-27 21:23:54 +0400

    first commit

---
+++ main5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void readingData () {
+	printf ("Reading data OK...\n");
+}
+void calcData () {
+	printf ("Calc. data OK...\n");
+}
+void writeData () {
+	printf ("Write data OK...\n");
+}
+int main () {
+	printf ("Test v10.01: OK\n");
+	readingData ();
+	calcData ();
+	writeData ();
+	return 0;
+}
+
+
